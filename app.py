114b2576b307b6a767cbf69c04d7accc6fe5f8b7
aditya04y <143684438+aditya04y@users.noreply.github.com>
2026-03-14 22:55:37 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+import cv2
+from PIL import Image
+
+# Load model
+model = tf.keras.models.load_model("pneumonia_model.keras")
+
+st.title("🫁 Pneumonia Detection from Chest X-ray")
+
+uploaded_file = st.file_uploader("Upload an X-ray image", type=["jpg","png","jpeg"])
+
+if uploaded_file is not None:
+
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded X-ray", use_container_width=True)
+
+    img = np.array(image)
+    img = cv2.resize(img,(224,224))
+    img = img/255.0
+    img = np.expand_dims(img,axis=0)
+
+    prediction = model.predict(img)[0][0]
+
+    if prediction > 0.5:
+        st.error("Prediction: Pneumonia")
+    else:
+        st.success("Prediction: Normal")
+
+    st.write("Confidence:", float(prediction))
